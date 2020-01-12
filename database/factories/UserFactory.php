8838876c478f23a5306801c8c958e98898c12b75
Aris Ripandi <ar.is@outlook.com>
2020-01-12 13:36:41 +0700

    fix users factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,5 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\Models\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
@@ -22,6 +21,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
+        'username' => $faker->unique()->userName,
         'password' => Hash::make('secret'),
         'remember_token' => Str::random(10),
     ];
