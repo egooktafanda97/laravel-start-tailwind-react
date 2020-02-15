de2224e2e93dd1035bb4a5ae0c3c730bf6585fc6
Aris Ripandi <ar.is@outlook.com>
2020-02-15 22:30:46 +0700

    add tailwind alpinejs scaffolding

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <!-- ASIDE -->
 <div id="sidebar"
-  class="w-48 md:w-64 border border-r text-sm h-screen bg-white shadow -mt-4 fixed md:relative hidden md:block"
+  class="w-48 md:w-64 border border-r text-sm h-screen bg-white shadow -mt-4 fixed md:relative"
   x-transition:enter="transition-transform transition-opacity ease-out duration-300"
   x-transition:enter="transition ease-out duration-300"
   x-transition:enter-start="opacity-0 transform -translate-x-2"
@@ -20,27 +20,27 @@
       </a>
     </div>
     <div>
-      <a class="block py-3 px-5 text-gray-800 hover:text-indigo-600 hover:font-semibold" href="#">
-        <i class="fas fa-fw fa-folder mr-2 text-gray-600 inline-block"></i> Facturas
+      <a class="block py-3 px-5 text-gray-800 hover:text-indigo-600 hover:font-semibold" href="javascript:;">
+        <i class="fas fa-fw fa-folder mr-2 text-gray-600 inline-block"></i> Services
       </a>
     </div>
     <div>
-      <a class="block py-3 px-5 text-gray-800 hover:text-indigo-600 hover:font-semibold" href="#">
+      <a class="block py-3 px-5 text-gray-800 hover:text-indigo-600 hover:font-semibold" href="javascript:;">
         <i class="fas fa-fw fa-briefcase mr-2 text-gray-600 inline-block"></i> Products
       </a>
     </div>
     <div>
-      <a class="block py-3 px-5 text-gray-800 hover:text-indigo-600 hover:font-semibold" href="#">
-        <i class="fas fa-fw fa-cogs mr-2 text-gray-600 inline-block"></i> Articles
+      <a class="block py-3 px-5 text-gray-800 hover:text-indigo-600 hover:font-semibold" href="javascript:;">
+        <i class="fas fa-fw fa-book-open mr-2 text-gray-600 inline-block"></i> Articles
       </a>
     </div>
     <div>
-      <a class="block py-3 px-5 text-gray-800 hover:text-indigo-600 hover:font-semibold" href="#">
-        <i class="fas fa-fw fa-bicycle mr-2 text-gray-600 inline-block"></i> Categories
+      <a class="block py-3 px-5 text-gray-800 hover:text-indigo-600 hover:font-semibold" href="javascript:;">
+        <i class="fas fa-fw fa-folder-open mr-2 text-gray-600 inline-block"></i> Categories
       </a>
     </div>
     <div>
-      <a class="block py-3 px-5 text-gray-800 hover:text-indigo-600 hover:font-semibold" href="#">
+      <a class="block py-3 px-5 text-gray-800 hover:text-indigo-600 hover:font-semibold" href="javascript:;">
         <i class="fas fa-fw fa-images mr-2 text-gray-600 inline-block"></i> Multimedia
       </a>
     </div>
@@ -51,13 +51,13 @@
       <div class="text-gray-700 font-semibold py-4 px-5">Settings</div>
     </div>
       <div>
-      <a class="block py-3 px-5 text-gray-800 hover:text-indigo-600 hover:font-semibold" href="#">
+      <a class="block py-3 px-5 text-gray-800 hover:text-indigo-600 hover:font-semibold" href="javascript:;">
         <i class="fas fa-fw fa-cogs mr-2 text-gray-600 inline-block"></i> Roles
       </a>
     </div>
     <div>
-      <a class="block py-3 px-5 text-gray-800 hover:text-indigo-600 hover:font-semibold" href="#">
-        <i class="fas fa-fw fa-lock mr-2 text-gray-600 inline-block"></i> Permisos
+      <a class="block py-3 px-5 text-gray-800 hover:text-indigo-600 hover:font-semibold" href="javascript:;">
+        <i class="fas fa-fw fa-users mr-2 text-gray-600 inline-block"></i> Users
       </a>
     </div>
 
@@ -65,16 +65,16 @@
   <!-- Third Aside Menu -->
   <div class="mt-4 pl-1">
     <div class="border-b border-gray-200 mb-2">
-      <div class="text-gray-700 font-semibold py-4 px-5">Informasi</div>
+      <div class="text-gray-700 font-semibold py-4 px-5">Support</div>
     </div>
     <div>
-      <a class="block py-3 px-5 hover:font-semibold text-gray-700" href="#">Planes</a>
+      <a class="block py-3 px-5 hover:font-semibold text-gray-700" href="javascript:;">Get Help</a>
     </div>
     <div>
-      <a class="block py-3 px-5 hover:font-semibold text-gray-700" href="#">Facturas</a>
+      <a class="block py-3 px-5 hover:font-semibold text-gray-700" href="javascript:;">Documentation</a>
     </div>
     <div>
-      <a class="block py-3 px-5 hover:font-semibold text-gray-700" href="#">Informaci&oacute;n</a>
+      <a class="block py-3 px-5 hover:font-semibold text-gray-700" href="javascript:;">Source Code</a>
     </div>
   </div>
 </div>
